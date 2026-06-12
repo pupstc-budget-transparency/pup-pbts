509b82b7c3044e831a14d1d3bc7a19fde44830cd
margarethcastillo <margarethcastillo938@gmail.com>
2026-06-12 16:51:18 +0800

    modified main.php

--- admin/main.php
+++ admin/main.php
@@ -14,9 +14,70 @@ $role = $_SESSION['role'];
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PUPSTC Admin Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
+
+<style>
+    .sidebar-btn {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        padding: 10px 15px;
+        border-radius: 30px;
+        text-decoration: none;
+        color: white;
+        background: transparent;
+        transition: all .3s ease;
+    }
+
+    .sidebar-btn span {
+        opacity: 1;
+        transform: translateX(0);
+        transition: .3s;
+    }
+
+    .sidebar-btn:hover {
+        background: white;
+        color: #7a0000;
+        transform: translateX(20px);
+        border-radius: 30px 0 0 30px;
+    }
+
+    .sidebar-btn.active {
+        background: white;
+        color: #7a0000;
+        transform: translateX(20px);
+        border-radius: 30px 0 0 30px;
+    }
+
+    .sidebar-btn {
+        position: relative;
+    }
+
+    .sidebar-btn:hover::after,
+    .sidebar-btn.active::after {
+        content: '';
+
+        position: absolute;
+
+        top: 0;
+        right: -40px;
+
+        width: 40px;
+        height: 100%;
+
+        background: white;
+    }
+
+    .sidebar {
+        width: 200px;
+        height: 100vh;
+        background: linear-gradient(180deg, #6b0000, #3d0000);
+        overflow: hidden;
+    }
+</style>
 
 <body>
     <div class="container-fluid text-white shadow-sm sticky-top" style="background-color: rgb(134,9,9);">
@@ -32,54 +93,58 @@ $role = $_SESSION['role'];
     <div class="container-fluid px-0">
         <div class="row g-0">
             <div class="col-12 col-xl-2">
-                <div class="d-flex flex-column gap-4 p-3 pt-5 bg-danger"
-                    style="background: linear-gradient(180deg, #6b0000 0%, #3d0000 100%); width: 180px; min-height: 100vh;">
+                <div class="sidebar d-flex flex-column gap-3 p-3 pt-5 position-sticky" style=" top: 0; height: 100vh;">
+
                     <?php
+
                     if ($role == 'super_admin') {
                         ?>
 
-                        <a href="dashboard.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Dashboard
-                        </a>
-                        <a href="budget-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Budget
-                        </a>
-                        <a href="expenditures.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Expenditures
-                        </a>
-                        <a href="projects.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Projects
-                        </a>
-                        <a href="reports.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Reports
-                        </a>
-                        <a href="feedback-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Feedback
-                        </a>
-                        <a href="voting-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Voting
+                        <a href="dashboard.php" class="sidebar-btn ">
+                            <i class="bi bi-speedometer2"></i>
+                            <span>Dashboard</span>
                         </a>
 
-                        <a href="users.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Users
+                        <a href="budget-management.php" class="sidebar-btn">
+                            <i class="bi bi-wallet2"></i>
+                            <span>Budget</span>
                         </a>
 
-                        <a href="audit-logs.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Audit Logs
+                        <a href="expenditures.php" class="sidebar-btn">
+                            <i class="bi bi-wallet2"></i>
+                            <span>Expenditures</span>
+                        </a>
+                        <a href="projects.php" class="sidebar-btn">
+                            <i class="bi bi-kanban"></i>
+                            <span>Projects</span>
                         </a>
 
-                        <a href="announcements.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Announcements
+                        <a href="reports.php" class="sidebar-btn">
+                            <i class="bi bi-file-earmark-bar-graph"></i>
+                            <span>Reports</span>
+                        </a>
+                        <a href="feedback-management.php" class="sidebar-btn">
+                            <i class="bi bi-envelope-paper"></i>
+                            <span>Feedback</span>
+                        </a>
+                        <a href="voting-management.php" class="sidebar-btn">
+                            <i class="bi bi-hand-index-thumb"></i>
+                            <span> Voting</span>
+                        </a>
+
+                        <a href="users.php" class="sidebar-btn active">
+                            <i class="bi bi-person-lines-fill"></i>
+                            <span> Users</span>
+                        </a>
+
+                        <a href="audit-logs.php" class="sidebar-btn">
+                            <i class="bi bi-clock-history"></i>
+                            <span>Audit Logs</span>
+                        </a>
+
+                        <a href="announcements.php" class="sidebar-btn">
+                            <i class="bi bi-megaphone-fill"></i>
+                            <span>Announcements</span>
                         </a>
 
                     <?php } ?>
@@ -89,24 +154,23 @@ $role = $_SESSION['role'];
                     <?php
                     if ($role == 'budget_officer') {
                         ?>
-                        <a href="dashboard.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Dashboard
+                        <a href="dashboard.php" class="sidebar-btn ">
+                            <i class="bi bi-speedometer2"></i>
+                            <span>Dashboard</span>
                         </a>
 
-                        <a href="budget-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Budget
+                        <a href="budget-management.php" class="sidebar-btn">
+                            <i class="bi bi-wallet2"></i>
+                            <span>Budget</span>
+                        </a>
+                        <a href="expenditures.php" class="sidebar-btn">
+                            <i class="bi bi-wallet2"></i>
+                            <span>Expenditures</span>
                         </a>
 
-                        <a href="expenditures.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Expenditures
-                        </a>
-
-                        <a href="reports.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Reports
+                        <a href="reports.php" class="sidebar-btn">
+                            <i class="bi bi-file-earmark-bar-graph"></i>
+                            <span>Reports</span>
                         </a>
 
                     <?php } ?>
@@ -116,23 +180,23 @@ $role = $_SESSION['role'];
                     <?php
                     if ($role == 'project_coordinator') {
                         ?>
-                        <a href="dashboard.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Dashboard
-                        </a>
-                        <a href="projects.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Projects
+                        <a href="dashboard.php" class="sidebar-btn ">
+                            <i class="bi bi-speedometer2"></i>
+                            <span>Dashboard</span>
                         </a>
 
-                        <a href="voting-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Voting
+                        <a href="projects.php" class="sidebar-btn">
+                            <i class="bi bi-kanban"></i>
+                            <span>Projects</span>
+                        </a>
+                        <a href="voting-management.php" class="sidebar-btn">
+                            <i class="bi bi-hand-index-thumb"></i>
+                            <span> Voting</span>
                         </a>
 
-                        <a href="reports.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Reports
+                        <a href="reports.php" class="sidebar-btn">
+                            <i class="bi bi-file-earmark-bar-graph"></i>
+                            <span>Reports</span>
                         </a>
 
                     <?php } ?>
@@ -142,23 +206,24 @@ $role = $_SESSION['role'];
                     <?php
                     if ($role == 'student_affairs') {
                         ?>
-                        <a href="dashboard.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Dashboard
-                        </a>
-                        <a href="users.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Students
+                        <a href="dashboard.php" class="sidebar-btn ">
+                            <i class="bi bi-speedometer2"></i>
+                            <span>Dashboard</span>
                         </a>
 
-                        <a href="feedback-management.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Feedback
+                        <a href="users.php" class="sidebar-btn">
+                            <i class="bi bi-mortarboard"></i>
+                            <span> Students</span>
                         </a>
 
-                        <a href="announcements.php" class="btn rounded-pill text-dark"
-                            style="background: rgba(255, 255, 255, 0.93); font-size:12px;">
-                            Announcements
+                        <a href="feedback-management.php" class="sidebar-btn">
+                            <i class="bi bi-envelope-paper"></i>
+                            <span>Feedback</span>
+                        </a>
+
+                        <a href="announcements.php" class="sidebar-btn">
+                            <i class="bi bi-megaphone-fill"></i>
+                            <span>Announcements</span>
                         </a>
 
                     <?php } ?>
@@ -185,7 +250,7 @@ $role = $_SESSION['role'];
                             </h2>
                             <p class="text-muted">
                                 Current Role:
-                                <strong>``
+                                <strong>
                                     <?php echo strtoupper(str_replace('_', ' ', $role)); ?>
                                 </strong>
                             </p>
